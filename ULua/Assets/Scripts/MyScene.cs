9447097e43915b5d8236e1c69ec0799b1d1bee49
刘家君 <591821945@qq.com>
2017-01-19 18:29:02 +0800

    创建ULua项目

---
+++ ULua/Assets/Scripts/MyScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using LuaInterface;
+
+public class MyScene : MonoBehaviour {
+
+    string mySource = @"
+MyTest.test();
+";
+
+    void Start () {
+        LuaScriptMgr luaMgr = new LuaScriptMgr();
+        luaMgr.Start();
+        LuaState l = luaMgr.lua;
+        l.DoString(mySource);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ULua/Assets/Scripts/MyTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static void test() {
+        print("由lua调用c#");
+    }
+}
